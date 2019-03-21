5069e888a3051b6de911bbcde0068035fc391533
DimaGashko <dgashko@mail.ua>
2019-03-21 23:57:50 +0200

    tsReport

---
+++ other/report/tsSolution/scripts/487--3279/487--3279.ts
@@ -0,0 +1,35 @@
+import getFrequencyOfNumbers from "./getFrequencyOfNumbers";
+
+(<any>window).getFrequencyOfNumbers = getFrequencyOfNumbers;
+
+console.log('%c * * * 487--3279 * * *', 'color: red');
+
+const inputNumbers = [
+   [
+      '4873279',
+      'ITS-EASY',
+      '888-4567',
+      '3-10-10-10',
+      '888-GLOP',
+      'TUT-GLOP',
+      '967-11-11',
+      '310-GINO',
+      'F101010',
+      '888-1200',
+      '-4-8-7-3-2-7-9-',
+      '487-3279',
+   ]
+]
+
+const res = inputNumbers.map((numbers, i) => {
+   const result = getFrequencyOfNumbers(numbers);
+   if (!result.length) return "No duplicates.";
+
+   return result.map((item) => {
+      return `${item.number} ${item.frequency}`;
+   }).join('\n');
+
+}).join('\n\n');
+
+console.log(res);
+
---
+++ other/report/tsSolution/scripts/clockHands/getAngleOfClockHands.ts
@@ -0,0 +1,15 @@
+export default function getAngleOfClockHands(timeStr: string): number { 
+   const time = timeStr.split(':').map(comp => parseInt(comp));
+   const h = time[0];
+   const m = time[1];
+
+   const hAngle = (360 / 12) * (h % 12) +
+		(360 / 12 / 60) * m;
+
+	const mAngle = (360 / 60) * (m % 60);
+
+	let angle = Math.abs(hAngle - mAngle);
+   if (angle > 180) angle = 360 - angle;
+   
+   return angle;
+}

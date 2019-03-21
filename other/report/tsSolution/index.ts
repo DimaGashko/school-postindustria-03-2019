2cf6992c8fed6415c41df73579f8461836742178
DimaGashko <dgashko@mail.ua>
2019-03-22 00:38:12 +0200

    clean

--- other/report/tsSolution/index.ts
+++
@@ -1,6 +0,0 @@
-import '../_templates/basePage/basePage';
-import './index.sass';
-
-import './scripts/minesweeper/minesweeper';
-import './scripts/487--3279/487--3279';
-import './scripts/clockHands/clockHands';

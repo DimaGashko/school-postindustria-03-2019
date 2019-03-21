ad7148c348a20f9981972e931d1a01561ffe8272
DimaGashko <dgashko@mail.ua>
2019-03-21 22:29:40 +0200

    optimizePath

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,6 +1,6 @@
 import '../_templates/basePage/basePage';
 import './index.sass';
 
-import './scripts/schoolPostIndustria2019/minesweeper/minesweeper';
-import './scripts/schoolPostIndustria2019/487--3279/487--3279';
-import './scripts/schoolPostIndustria2019/clockHands/clockHands';
+import './scripts/minesweeper/minesweeper';
+import './scripts/487--3279/487--3279';
+import './scripts/clockHands/clockHands';

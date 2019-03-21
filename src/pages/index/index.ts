be44e364ecc13d841eec1b543a07f58a57aa14cf
DimaGashko <dgashko@mail.ua>
2019-03-21 21:03:50 +0200

    testsForNumbers

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -2,10 +2,21 @@ import '../_templates/basePage/basePage';
 import './index.sass';
 
 import './scripts/schoolPostIndustria2019/minesweeper/minesweeper';
-import { numberToStandardForm } from './scripts/schoolPostIndustria2019/487--3279/getFrequencyOfNumbers';
+import getFrequencyOfNumbers from './scripts/schoolPostIndustria2019/487--3279/getFrequencyOfNumbers';
 
-console.log(numberToStandardForm(""));
-console.log(numberToStandardForm("00-00-00"));
-console.log(numberToStandardForm("ITS-EASY"));
-console.log(numberToStandardForm("888-GLOP"));
-console.log(numberToStandardForm("3-10-10-10"));
+const res = getFrequencyOfNumbers([
+   '4873279',
+   'ITS-EASY',
+   '888-4567',
+   '3-10-10-10',
+   '888-GLOP',
+   'TUT-GLOP',
+   '967-11-11',
+   '310-GINO',
+   'F101010',
+   '888-1200',
+   '-4-8-7-3-2-7-9-',
+   '487-3279',
+]);
+
+console.log(res);

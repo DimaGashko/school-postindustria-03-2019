dd43da3e9dd895669a0a08be3c2e6011752b625e
DimaGashko <dgashko@mail.ua>
2019-03-21 21:13:51 +0200

    NumbersTaskDone

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -2,21 +2,5 @@ import '../_templates/basePage/basePage';
 import './index.sass';
 
 import './scripts/schoolPostIndustria2019/minesweeper/minesweeper';
-import getFrequencyOfNumbers from './scripts/schoolPostIndustria2019/487--3279/getFrequencyOfNumbers';
-
-const res = getFrequencyOfNumbers([
-   '4873279',
-   'ITS-EASY',
-   '888-4567',
-   '3-10-10-10',
-   '888-GLOP',
-   'TUT-GLOP',
-   '967-11-11',
-   '310-GINO',
-   'F101010',
-   '888-1200',
-   '-4-8-7-3-2-7-9-',
-   '487-3279',
-]);
-
-console.log(res);
+import './scripts/schoolPostIndustria2019/487--3279/487--3279';
+//import './scripts/schoolPostIndustria2019/clockHours/clockHours';
--- src/pages/index/scripts/schoolPostIndustria2019/487--3279/487--3279.ts
+++ src/pages/index/scripts/schoolPostIndustria2019/487--3279/487--3279.ts
@@ -1,25 +1,34 @@
-import fillMinesMap from "./fillMinesMap";
+import getFrequencyOfNumbers from "./getFrequencyOfNumbers";
 
-(<any>window).fillMinesMap = fillMinesMap;
+(<any>window).fillMinesMap = getFrequencyOfNumbers;
 
 console.log('%c * * * 487--3279 * * *', 'color: red');
 
-const emptyMaps = [
+const inputNumbers = [
    [
-      '*...',
-      '....',
-      '.*..',
-      '....',
-   ], [
-      '**...',
-      '.....',
-      '.*...',
-   ],
+      '4873279',
+      'ITS-EASY',
+      '888-4567',
+      '3-10-10-10',
+      '888-GLOP',
+      'TUT-GLOP',
+      '967-11-11',
+      '310-GINO',
+      'F101010',
+      '888-1200',
+      '-4-8-7-3-2-7-9-',
+      '487-3279',
+   ]
 ]
 
-const res = emptyMaps.map((emptyMap, i) => {
-   const resultMap = fillMinesMap(emptyMap);
-   return `Field #${i + 1}\n${resultMap.join('\n')}`;
+const res = inputNumbers.map((numbers, i) => {
+   const result = getFrequencyOfNumbers(numbers);
+   if (!result.length) return "No duplicates.";
+
+   return result.map((item) => {
+      return `${item.number} ${item.frequency}`;
+   }).join('\n');
+   
 }).join('\n\n');
 
 console.log(res);
